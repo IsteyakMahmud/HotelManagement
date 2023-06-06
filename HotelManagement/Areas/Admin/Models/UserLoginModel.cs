5fd3811b6968c6e5a2aff76614cada676433aa3d
isteyak <isteyakmahmud@gmail.com>
2023-06-07 02:01:59 +0600

    user profile system done

---
+++ HotelManagement/Areas/Admin/Models/UserLoginModel.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotelManagement.Areas.Admin.Models
+{
+    public class UserLoginModel
+    {
+        [Required]
+        [EmailAddress]
+        public string UEmail { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string UPass { get; set; }
+
+        [Display(Name = "Remember me?")]
+        public bool RememberMe { get; set; }
+
+    }
+}
